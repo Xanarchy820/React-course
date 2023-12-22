7a75aefda73fb49238f5f42946e14306d8ac04d1
Xanarchy820 <55326894sg@gmail.com>
2023-12-21 21:08:16 -0600

    base update

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://Xanarchy820.github.io/React-course"
 })
